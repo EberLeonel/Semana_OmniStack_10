80cf698910cb125dadd37c0ca78123fb6382cccc
eber <eber@maisdados.com.br>
2020-01-14 17:02:11 -0300

    Second day lesson: created routes, models,utils.

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:Omnistack@123@cluster0-sbjk5.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+//Médodos HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parâmetros:
+
+//Query Params: request.query (Filtros, ordenação, paginação, ...)
+//Route Params: request.params (identificar um recurso na alteração ou remoção)
+
+//Body: request.body (Dados para criação ou alteração de um registro)
+
+//MongoDB (não-relacional)
+
+app.listen(1717);
