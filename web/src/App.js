286ac457eaf15d3fe19eeae2cfcc5f4a84325202
Eber <eber.dto@gmail.com>
2020-01-15 23:53:41 -0300

    Third day lesson: created the  frontend basic structure.

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//Componente: bloco isolado de html, css, js, o qual não interfere no restante da aplicação
+//Estado: informações mantidas pelo componente (Lembrar: conceito de imutabilidade)
+//Propriedade: informações que um componente pai passa para o componente filho
+
+function App() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+export default App;
